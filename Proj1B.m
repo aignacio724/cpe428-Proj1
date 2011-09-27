a0c3a357453355212ef95b739b86c87df8bbf2f8
aignacio <aignacio724@gmail.com>
2011-09-27 09:36:41 -0700

    Calculated area for each bacteria

--- Proj1B.m
+++ Proj1B.m
@@ -8,16 +8,34 @@ subplot(224), imshow(x < 102), title('Threshold 102');
 
 y = x < 102;
 figure, imshow(y);
-
-z = bwboundaries(y);
-figure, imshow(y);
-text(16,16,strcat('\color{red}Objects Found:',num2str(length(z))))
+m = bwboundaries(y);
+text(16,16,strcat('\color{red}Objects Found:',num2str(length(m))))
 
 hold on
 
-for k = 1:length(z)
-boundary = z{k};
+for j = 1:length(m)
+boundary = m{j};
 plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 0.2)
 end
-bwarea(y)
+
+
+disp('Number of bacteria in image')
+num = length(m)
+
+z = zeros(1,num);
+for k = 1:numel(B)
+    if B(k) > 0
+        z(B(k)) = z(B(k))+1;
+    end
+end
+
+%A
+disp('Area of each Bacteria')
+z;
+disp('Area of all Bacteria')
+sum(z)
+
+A = bwlabel(y, 8)
+cc = bwconncomp(A)
+%hold on
 whos
