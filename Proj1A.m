a5de317dc9b2d66230619c9cd49cbd833c771c2b
aignacio <aignacio724@gmail.com>
2011-09-26 22:14:37 -0700

    Adding Pictures and matlab files for project 1

---
+++ Proj1A.m
@@ -0,0 +1,28 @@
+Y = imread('C:\Users\aignacio\Documents\MATLAB\Landscape.jpg');
+X = imread('C:\Users\aignacio\Documents\MATLAB\HiggsBoson.jpg');
+tempX = rgb2gray(X);
+tempY = rgb2gray(Y);
+a = imresize(Y, 0.01);
+b = imresize(X, 0.01);
+
+figure, imshow(X), title('Group Picture');
+figure, imshow(Y), title('Landscape');
+figure, imshow(a), title('Scaled down 99%');
+figure, imshow(b), title('Scaled down 99%');
+
+figure
+subplot(2,2,3), imshow(X), title('Original');
+subplot(2,2,4), imshow(tempX), title('Gray Scale');
+subplot(2,2,1:2), imhist(tempX), title('Histogram');
+
+
+figure
+subplot(2,2,3), imshow(Y), title('Original');
+subplot(2,2,4), imshow(tempY), title('Gray Scale');
+subplot(2,2,1:2), imhist(tempY), title('Histogram');
+
+max(tempX)
+min(tempX)
+max(tempY)
+min(tempY)
+who
---
+++ Proj1B.m
@@ -0,0 +1,21 @@
+x = imread('bacteria.bmp');
+
+figure
+subplot(221), imshow(x), title('Original');
+subplot(222), imshow(x < 150), title('Threshold 150');
+subplot(223), imshow(x < 110), title('Threshold 110');
+subplot(224), imshow(x < 102), title('Threshold 102');
+
+y = x < 102;
+figure, imshow(y);
+
+z = bwboundaries(y);
+figure, imshow(y);
+text(16,16,strcat('\color{red}Objects Found:',num2str(length(z))))
+hold on
+
+for k = 1:length(z)
+boundary = z{k};
+plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 0.2)
+end
+whos
