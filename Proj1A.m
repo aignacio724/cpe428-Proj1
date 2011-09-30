b21dce1665d3eafee8ce0ba9fee759b46c14fd2c
aignacio <aignacio724@gmail.com>
2011-09-29 17:18:08 -0700

    Changed the Minimum and Maximum funcionts

--- Proj1A.m
+++ Proj1A.m
@@ -2,27 +2,36 @@ Y = imread('Landscape.jpg');
 X = imread('HiggsBoson.jpg');
 tempX = rgb2gray(X);
 tempY = rgb2gray(Y);
-a = imresize(Y, 0.01);
-b = imresize(X, 0.01);
+a = imresize(Y, 0.1);
+b = imresize(X, 0.1);
 
 figure, imshow(X), title('Group Picture');
 figure, imshow(Y), title('Landscape');
-figure, imshow(a), title('Scaled down 99%');
-figure, imshow(b), title('Scaled down 99%');
+figure, imshow(a), title('Scaled down 90%');
+figure, imshow(b), title('Scaled down 90%');
 
+%Group Picture
 figure
 subplot(2,2,3), imshow(X), title('Original');
 subplot(2,2,4), imshow(tempX), title('Gray Scale');
 subplot(2,2,1:2), imhist(tempX), title('Histogram');
 
-
+%Landscape Image 
 figure
 subplot(2,2,3), imshow(Y), title('Original');
 subplot(2,2,4), imshow(tempY), title('Gray Scale');
 subplot(2,2,1:2), imhist(tempY), title('Histogram');
 
-max(tempX)
-min(tempX)
-max(tempY)
-min(tempY)
-who
+%Group Picture
+[maxValue maxLocation] = max(tempX(:))
+[maxRow maxCol] = ind2sub(size(tempX), maxLocation) 
+[minValue minLocation] = min(tempX(:))
+[minRow minCol] = ind2sub(size(tempX), minLocation)
+
+%Landscape Image 
+[maxValue maxLocation] = max(tempY(:))
+[maxRow maxCol] = ind2sub(size(tempY), maxLocation) 
+[minValue minLocation] = min(tempY(:))
+[minRow minCol] = ind2sub(size(tempY), minLocation)
+
+whos
